20f76ebd7ea3fb48fee5c7d1efaaaf50fefd8bec
a-jwc <chauajw@gmail.com>
2022-03-06 02:07:54 -0800

    add size and toppings state

--- src/features/order/orderSlice.js
+++ src/features/order/orderSlice.js
@@ -8,6 +8,10 @@ export const orderSlice = createSlice({
 		order: "",
 		hasName: false,
 		hasPhoneNumber: false,
+		size: "",
+		hasSize: false,
+		toppings: [],
+		hasToppings: false,
 	},
 	reducers: {
 		updateName: (state, action) => {
@@ -25,6 +29,18 @@ export const orderSlice = createSlice({
 		updateHasPhoneNumber: (state, action) => {
 			state.hasPhoneNumber = action.payload;
 		},
+		updateSize: (state, action) => {
+			state.size = action.payload;
+		},
+		updateHasSize: (state, action) => {
+			state.hasSize = action.payload;
+		},
+		updateToppings: (state, action) => {
+			state.toppings = action.payload;
+		},
+		updateHasToppings: (state, action) => {
+			state.hasToppings = action.payload;
+		},
 	},
 });
 
@@ -34,12 +50,21 @@ export const {
 	updatePhoneNumber,
 	updateHasName,
 	updateHasPhoneNumber,
+	updateSize,
+	updateHasSize,
+	updateToppings,
+	updateHasToppings,
 } = orderSlice.actions;
 
 export const selectName = (state) => state.order.name;
 export const selectPhoneNumber = (state) => state.order.phoneNumber;
-export const selectOrder = (state) => state.order.order;
 export const selectHasName = (state) => state.order.hasName;
 export const selectHasPhoneNumber = (state) => state.order.hasPhoneNumber;
+
+export const selectOrder = (state) => state.order.order;
+export const selectSize = (state) => state.order.size;
+export const selectHasSize = (state) => state.order.hasSize;
+export const selectToppings = (state) => state.order.toppings;
+export const selectHasToppings = (state) => state.order.hasToppings;
 
 export default orderSlice.reducer;
